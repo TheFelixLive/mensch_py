c181a9a979bd0889ddb13abc88f3ba66d922d80f
TheFelixLive <jouloukas@gmail.com>
2025-05-01 13:03:05 +0200

    tutorial

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,37 +1,52 @@
 import pygame
+from sys import exit
 
 pygame.init()
-window = pygame.display.set_mode((640,480))
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Testing")
 
-def key_debug():
-    print(pygame.event.get())
+clock = pygame.time.Clock()
 
+# Window icon
+icon = pygame.image.load('ui/icon.png') 
+pygame.display.set_icon(icon)
 
-x = 120
-y = 120
-running = True
-while running:
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
+sky_surface = pygame.image.load("graphics/Sky.png").convert()
+ground_surface = pygame.image.load("graphics/ground.png").convert()
+text_surface = test_font.render("My game", False, "black")
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600,300))
+
+player_surface = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80,300))
+
+while True:
+    # Inputs
     for event in pygame.event.get():
-        key_debug()
+        # Quitting
         if event.type == pygame.QUIT:
-            running = False
-            break
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-                break
-            elif event.key == pygame.K_RIGHT:
-                x += 8
-            elif event.key == pygame.K_LEFT:
-                x -= 8
-            elif event.key == pygame.K_DOWN:
-                y += 8
-            elif event.key == pygame.K_UP:
-                y -= 8
+            pygame.quit()
+            exit()
 
-    window.fill((0,0,0))
-    pygame.draw.rect(window,(0,0,255),(x,y,400,240))
+    
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+    
+
+    screen.blit(snail_surface,snail_rect)
+    if snail_rect.right > 0:
+        snail_rect.x -= 1
+    else:
+        snail_rect.right = 800
+
+    player_rect.left += 1
+    screen.blit(player_surface,player_rect)
+
     pygame.display.update()
+    clock.tick(60)
 
-pygame.quit()
+# https://youtu.be/AY9MnQ4x3zk?t=3832
